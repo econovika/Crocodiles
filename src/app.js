f811ac90604c01ff1ba1ae55a4d301cc3c007526
klntsky <klntsky@gmail.com>
2019-10-27 17:32:40 +0300

    App

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-alert("Hello, world!");
+class App {
+  constructor () {
+    this.state = {
+    };
+
+    document.getElementById('menu-box').style.display = 'block';
+
+    function hideAllContainers() {
+      [].forEach.call(
+        document.querySelectorAll('.container'),
+        el => el.style.display = 'none'
+      );
+    }
+
+    [].forEach.call(
+      document.querySelectorAll('.container-select'),
+      el => {
+        el.addEventListener('click', evt => {
+          hideAllContainers();
+          const boxId = evt.target.id.split('-')[1];
+          const box = document.getElementById(boxId + '-box');
+          box.style.display = 'block';
+        });
+      }
+    );
+    document.getElementById('menu-box').style.display = 'block';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => new App());
